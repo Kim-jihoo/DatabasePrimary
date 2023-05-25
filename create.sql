389a41c168b4548ca4138aa6b5427e9e217772d7
Kim-jihoo <109203288+Kim-jihoo@users.noreply.github.com>
2023-05-26 02:40:45 +0900

    Create create.sql
    menu table의 name에 unique 사용

---
+++ create.sql
@@ -0,0 +1,29 @@
+CREATE TABLE users (
+  id VARCHAR(255) PRIMARY KEY,
+  pw VARCHAR(255)
+);
+
+CREATE TABLE restaurant (
+  name VARCHAR(255) PRIMARY KEY,
+  area VARCHAR(255),
+  category VARCHAR(255)
+);
+
+CREATE TABLE menu (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(255) UNIQUE,
+  restaurant VARCHAR(255),
+  price INT
+);
+
+CREATE TABLE review (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  user_id VARCHAR(255),
+  restaurant VARCHAR(255),
+  menu VARCHAR(255),
+  comment VARCHAR(255),
+  rate DECIMAL(3,1) CHECK (rate >= 0 AND rate <= 5),
+  FOREIGN KEY (user_id) REFERENCES users(id),
+  FOREIGN KEY (restaurant) REFERENCES restaurant(name),
+  FOREIGN KEY (menu) REFERENCES menu(name)
+);
